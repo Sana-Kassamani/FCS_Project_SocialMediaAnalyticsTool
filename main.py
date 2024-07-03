63f44787d3b2647d66615a34a07ea2f5657f1cc7
Sana <Lenovo@DESKTOP-I4OODFD>
2024-07-04 02:18:02 +0300

    Added-- main.py to test User functions

---
+++ main.py
@@ -0,0 +1,26 @@
+from User import User
+
+def main():
+    user1=User("jack")
+    user1.addGenre("Thriller")
+    user1.addGenre("Comedy")
+    user1.addGenre("Sci-Fi")
+    user1.displayUser()
+    user1.addBooksRead("Harry Potter and The Prisonor of Azkaban")
+    user1.addBooksRead("Betrayed")
+    user1.displayUser()
+    user1.deleteBooksRead("betrayed")
+    user1.addBooksCurrentlyReading("The Inner Circle")
+    user1.addBooksToBeRead("How The Light Gets In")
+    user1.displayUser()
+    user1.setName("Jacques")
+    user1.displayUser()
+    user2=User("Mary")
+    user2.displayUser()
+    user1.deleteUser()
+    user3=User("Ali")
+    user3.displayUser()
+    user4=User("ahmed")
+    user4.displayUser()
+
+main()
